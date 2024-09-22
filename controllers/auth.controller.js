3338f90be53e2711ff7f5ad5726da21bc5854178
Michael <msalerno@gmail.com>
2024-09-22 18:56:51 -0400

    working on setting up routes for signup login logout

---
+++ controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup = async(req, res) => {
+    try {
+        const {fullName, username, password, confirmPassword, gender} = req.body
+    } catch (error) {
+        
+    }
+}
+
+export const login = (req, res) => {
+    console.log("Login User");
+    res.send("Login");
+
+};
+
+export const logout = (req, res) => {
+    console.log("Logout User");
+    res.send("Logout");
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
-router.get("/login", (req, res) => {
-    res.send("Login Route")
-})
+router.post("/login", login )
+
+router.post("/signup", signup );
+
+router.post("/logout", logout );
 
 export default router
